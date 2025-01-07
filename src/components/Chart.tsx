a012a274c3c54c7270d0d8bca813ba1a0b188aee
lostfictions <lostfictions@users.noreply.github.com>
2025-01-07 03:02:29 -0500

    WIP: split to separate files, server render d3 version, add colour palette

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,17 +1,4 @@
-"use client";
-
-import { useMemo } from "react";
-import {
-  Axis,
-  darkTheme,
-  GlyphSeries,
-  LineSeries,
-  XYChart,
-} from "@visx/xychart";
 import * as d3 from "d3";
-
-import { Tooltip, TooltipContent, TooltipTrigger } from "./Tooltip";
-
 import type { EmojiByDate } from "~/app/server/[guild]/page";
 import type { CSSProperties } from "react";
 
@@ -34,97 +21,6 @@ const fmt = new Intl.DateTimeFormat("en-US", {
   year: "numeric",
 });
 
-export function ChartVis({ data }: { data: EmojiByDate }) {
-  const offsetsForDateAndId = useMemo(() => {
-    const o: Record<string, number> = {};
-    const byDate = Object.values(groupBy(data, (d) => `${d.day} ${d.count}`));
-    for (const group of byDate) {
-      for (let i = 0; i < group.length; i++) {
-        const e = group[i];
-        o[`${e.day} | ${e.id}`] = i + 1;
-      }
-    }
-    return o;
-  }, [data]);
-
-  const grouped = useMemo(
-    () =>
-      Object.entries(
-        groupBy(
-          data.map((d) => ({
-            ...d,
-            // convert bigint to number
-            count: Number(d.count),
-          })),
-          (d) => d.id,
-        ),
-      ),
-    [data],
-  );
-
-  return (
-    <XYChart
-      captureEvents={false}
-      // complains on the server if it doesn't have fixed width and height
-      // but doesn't seem to server render even if they're provided, so...
-      // height={400}
-      // width={500}
-      xScale={{ type: "band" }}
-      yScale={{ type: "linear" }}
-      theme={darkTheme}
-    >
-      <Axis orientation="bottom" />
-      <Axis orientation="left" />
-      {grouped.map(([id, datum]) => (
-        <LineSeries
-          key={id}
-          dataKey={id}
-          data={datum}
-          xAccessor={(d) => d.day}
-          yAccessor={(d) => d.count}
-        />
-      ))}
-      {/* we need to map over grouped a second time to ensure the images are after (ie. on top of) the lines */}
-      {grouped.map(([id, datum]) => (
-        <GlyphSeries
-          key={id}
-          dataKey={id}
-          data={datum}
-          xAccessor={(d) => d.day}
-          yAccessor={(d) => d.count}
-          renderGlyph={({ datum: e, x, y, color }) => (
-            <Tooltip>
-              <TooltipTrigger>
-                <image
-                  x={x - 8 + offsetsForDateAndId[`${e.day} | ${e.id}`] * 18}
-                  y={y - 8}
-                  className="size-4 object-contain"
-                  href={`https://cdn.discordapp.com/emojis/${e.id}.png`}
-                />
-              </TooltipTrigger>
-              <TooltipContent arrowClass="fill-slate-900">
-                <div className="flex flex-col items-center gap-1 rounded bg-slate-900 px-4 py-2 text-white">
-                  <div className="flex items-center gap-2">
-                    <img
-                      className="size-4 object-contain"
-                      src={`https://cdn.discordapp.com/emojis/${e.id}.png`}
-                    />
-                    <div style={{ color }}>:{e.name}:</div>
-                  </div>
-                  <div>
-                    Used {e.count} {e.count === 1 ? "time" : "times"} on{" "}
-                    {fmt.format(new Date(e.day))}
-                  </div>
-                </div>
-              </TooltipContent>
-            </Tooltip>
-          )}
-        />
-      ))}
-    </XYChart>
-  );
-}
-
 // based on https://buildui.com/recipes/responsive-line-chart
 export function Chart({ data }: { data: EmojiByDate }) {
   /**
@@ -132,7 +28,7 @@ export function Chart({ data }: { data: EmojiByDate }) {
    * B might both have 3 uses on december 15). this calculates a position offset
    * for the glyph to disambiguate them.
    */
-  const offsetsForDateAndId = useMemo(() => {
+  const offsetsForDateAndId = (() => {
     const o: Record<string, number> = {};
     const byDate = Object.values(groupBy(data, (d) => `${d.day} ${d.count}`));
     for (const group of byDate) {
@@ -142,28 +38,27 @@ export function Chart({ data }: { data: EmojiByDate }) {
       }
     }
     return o;
-  }, [data]);
+  })();
 
-  // FIXME: be consistent -- memoize or don't, use a single loop if we can...
-  const [grouped, minDate, maxDate] = useMemo(
-    () =>
-      [
-        Object.entries(
-          groupBy(
-            data.map((d) => ({
-              ...d,
-              day: new Date(d.day),
-              // convert bigint to number
-              count: Number(d.count),
-            })),
-            (d) => d.id,
-          ),
-        ),
-        new Date(Math.min(...data.map((d) => new Date(d.day).getTime()))),
-        new Date(Math.max(...data.map((d) => new Date(d.day).getTime()))),
-      ] as const,
-    [data],
+  const grouped = Object.entries(
+    groupBy(
+      data.map((d) => ({
+        ...d,
+        day: new Date(d.day),
+        // convert bigint to number
+        count: Number(d.count),
+      })),
+      (d) => d.id,
+    ),
   );
+  const minDate = new Date(
+    Math.min(...data.map((d) => new Date(d.day).getTime())),
+  );
+  const maxDate = new Date(
+    Math.max(...data.map((d) => new Date(d.day).getTime())),
+  );
+
+  const colorScale = d3.scaleOrdinal().range(d3.schemeSet3);
 
   const xScale = d3.scaleUtc().domain([minDate, maxDate]).range([0, 100]);
 
@@ -273,8 +168,7 @@ export function Chart({ data }: { data: EmojiByDate }) {
               key={id}
               d={d}
               fill="none"
-              className="text-gray-600"
-              stroke="currentColor"
+              stroke={colorScale(id) as string}
               strokeWidth="2"
               vectorEffect="non-scaling-stroke"
             />
@@ -288,8 +182,7 @@ export function Chart({ data }: { data: EmojiByDate }) {
               strokeWidth="8"
               strokeLinecap="round"
               fill="none"
-              stroke="currentColor"
-              className="text-gray-400"
+              stroke={colorScale(d.id) as string}
             />
           ))}
         </svg>
